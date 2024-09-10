c2e223b06454a943f830573bf04d6df9380c1dcf
steve richard <steve.richard3592@gmail.com>
2024-09-10 11:06:29 -0700

    First Commit

---
+++ core/src/main/java/io/steve/comp452/Main.java
@@ -0,0 +1,13 @@
+package io.steve.comp452;
+
+import com.badlogic.gdx.Game;
+
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends Game {
+
+    @Override
+    public void create() {
+        
+    }
+}
