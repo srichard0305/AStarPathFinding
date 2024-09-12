f7f667e569390348b32bfa920eae92ddb3793f27
steve richard <steve.richard3592@gmail.com>
2024-09-12 16:11:05 -0700

    Added tile map and assets

---
+++ core/src/main/java/io/steve/comp452/GameScreen.java
@@ -0,0 +1,139 @@
+package io.steve.comp452;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+
+public class GameScreen implements Screen {
+
+    Game game;
+    TiledMap map;
+    TiledMapTileLayer tiledMapTileLayerTerrain, tiledMapTileLayerAnt;
+    OrthogonalTiledMapRenderer renderer;
+    OrthographicCamera camera;
+    Stage stage;
+    Viewport viewport;
+
+    final int TILE_WIDTH = 50;
+    final int TILE_HEIGHT = 50;
+    final int ROW = 16;
+    final int COL = 16;
+
+    Table menu;
+    Button openTerrain, grassTerrain, swampTerrain, obstacle, start, restart;
+    Table clickableActors;
+
+
+    GameScreen(Game game){
+        this.game = game;
+
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        viewport = new FillViewport(camera.viewportWidth, camera.viewportHeight);
+        stage = new Stage(viewport);
+        menu = new Table();
+        menu.setFillParent(true);
+        menu.right();
+
+        map = new TiledMap();
+        tiledMapTileLayerTerrain = new TiledMapTileLayer(ROW, COL,TILE_WIDTH, TILE_HEIGHT);
+        map.getLayers().add(tiledMapTileLayerTerrain);
+        for(int i  = 0; i < ROW; i++){
+            for(int j  = 0; j < COL; j++){
+                Texture landTexture = new Texture(Gdx.files.internal("square.png"));
+                TextureRegion landTextureReg = new TextureRegion(landTexture);
+                StaticTiledMapTile myTile = new StaticTiledMapTile(landTextureReg);
+                TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
+                cell.setTile(myTile);
+                tiledMapTileLayerTerrain.setCell(i, j, cell);
+            }
+        }
+
+        renderer = new OrthogonalTiledMapRenderer(map);
+
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = new BitmapFont();
+        textButtonStyle.fontColor = Color.BLACK;
+
+        openTerrain = new TextButton("Open Terrain", textButtonStyle);
+        grassTerrain = new TextButton("Grass Terrain", textButtonStyle);
+        swampTerrain = new TextButton("Swamp Terrain", textButtonStyle);
+        obstacle = new TextButton("Obstacle Terrain", textButtonStyle);
+        start = new TextButton("START", textButtonStyle);
+        restart =  new TextButton("restart", textButtonStyle);
+
+        menu.add(openTerrain).padRight(50f).padBottom(25f);
+        menu.row();
+        menu.add(grassTerrain).padRight(50f).padBottom(25f);
+        menu.row();
+        menu.add(swampTerrain).padRight(50f).padBottom(25f);
+        menu.row();
+        menu.add(obstacle).padRight(50f).padBottom(25f);
+        menu.row();
+        menu.add(start).padRight(50f).padBottom(25f);
+        menu.row();
+        menu.add(restart).padRight(50f).padBottom(25f);
+
+
+        stage.addActor(menu);
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(222/225f,184/225f,135/225f, 1);
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
+        stage.draw();
+
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
--- core/src/main/java/io/steve/comp452/Main.java
+++ core/src/main/java/io/steve/comp452/Main.java
@@ -6,8 +6,39 @@ import com.badlogic.gdx.Game;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
 
+    GameScreen gameScreen;
+
     @Override
     public void create() {
-        
+        gameScreen = new GameScreen(this);
+        setScreen(gameScreen);
     }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        gameScreen.dispose();
+    }
+
+    @Override
+    public void pause() {
+        super.pause();
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gameScreen.resize(width, height);
+    }
+
+
 }
